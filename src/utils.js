23c2cbaa49e516eacf1c6abc452ceb4796858ec8
kirkdotcam <camden.kirkland@gmail.com>
2024-10-03 07:57:18 -0500

    add faker and utils

---
+++ src/utils.js
@@ -0,0 +1,132 @@
+import {faker} from "@faker-js/faker"
+
+/**
+ * Generates a dataset of specified type and size.
+ *
+ * @param {Object} params - The parameters for generating the dataset.
+ * @param {number} params.numDocs - The total number of documents to generate.
+ * @param {number} params.numBatches - The number of batches to split the data into.
+ * @param {string} params.typeOfData - The type of data to generate ('person', 'recipe', 'foodPairing').
+ * @param {Object} params.additionalParams - Additional parameters to pass to the generation methods.
+ * @returns {Array<Array<Object>>} An array containing batches of generated data.
+ */
+function generateDataset({
+  numDocs, 
+  numBatches, 
+  typeOfData,
+  additionalParams
+  }){
+  
+  let batchSize = numDocs / numBatches
+ 
+  let allData = [] 
+
+  let generateMethod 
+
+  switch(typeOfData){
+    case "person":
+      generateMethod = createPerson
+      break
+    case "recipe":
+      generateMethod = createRecipe
+      break
+    case "foodPairing":
+      generateMethod = createPairsWith
+      break
+    default:
+      generateMethod = createPerson
+  }
+
+  for (let batch of [...Array(numBatches)] ){
+    let generatedData = [...Array(batchSize)].map(()=> generateMethod(additionalParams))
+
+    allData.push(generatedData)
+  }
+
+  return allData
+}
+
+/**
+ * Creates a random address object.
+ *
+ * @returns {Object} An object representing a random address.
+ */
+function createAddress(){
+  return {
+    address: faker.location.streetAddress(),
+    location: [
+      faker.location.latitude(),
+      faker.location.longiutude()
+    ],
+    city: faker.location.city(),
+    state: faker.location.state(),
+    zip: faker.location.zipCode(),
+  }
+}
+
+/**
+ * Creates a random person object.
+ *
+ * @param {number} [numWallets=4] - The number of Ethereum wallets to generate for the person.
+ * @param {number} [numAddresses=1] - The number of historical addresses to generate for the person.
+ * @returns {Object} An object representing a random person.
+ */
+function createPerson({numWallets = 4, numAddresses} ){
+  let newPerson = {
+  name: faker.person.fullName(),
+  gender: faker.person.gender(),
+  sex: faker.person.sex(),
+  occupation: faker.person.jobTitle(),
+  bio: faker.person.bio(),
+  ethAccounts: [...Array(numWallets)].map(()=>faker.finance.ethereumAddress())
+  }
+ 
+  if (numAddresses > 1){
+    newPerson.historicalAddresses = [...Array(numAddresses)].map(()=> createAddress())
+    newPerson.currentAddress = newPerson.historicalAddresses[0]
+  } else {
+    newPerson.currentAddress = createAddress() 
+  }
+  return newPerson
+}
+
+
+/**
+ * Creates a random recipe object.
+ *
+ * @param {number} [numVerbs=4] - The number of description verbs to generate for the recipe.
+ * @returns {Object} An object representing a random recipe.
+ */
+function createRecipe({numVerbs=4}){
+  return{
+    item: faker.food.dish(),
+    cuisine: faker.food.ethnicCategory(),
+    description: faker.food.description(),
+    descriptionVerbs: [...Array(numVerbs)].map(()=> faker.food.adjective()),
+    garnishOrSpin: [...Array(5)].map(()=> faker.food.ingredient())
+
+  }
+  
+}
+
+/**
+ * Creates a random pairing object.
+ *
+ * @param {number} [pairsWithDegree=1] - The number of ingredients to pair with.
+ * @returns {Object} An object representing a random ingredient pairing.
+ */
+function createPairsWith(pairsWithDegree=1){
+  return {
+    item: faker.food.ingredient(),
+    pairsWith: [...Array(pairsWithDegree)].map(()=>faker.food.ingredient())
+  }
+}
+
+export {
+  generateDataset,
+  createAddress,
+  createPairsWith,
+  createPerson,
+  createRecipe
+}
+
