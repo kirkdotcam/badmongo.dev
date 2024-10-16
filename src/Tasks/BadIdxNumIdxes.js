3fbeb1b3d82aa8687f043f36d272d98bf81bb670
kirkdotcam <camden.kirkland@gmail.com>
2024-10-16 17:07:54 -0500

    finish draft of bad num idxes

--- src/Tasks/BadIdxNumIdxes.js
+++ src/Tasks/BadIdxNumIdxes.js
@@ -1,37 +1,94 @@
-import {Task} from "../Task.js"
-import {createRandomIndexDoc, generateDataset, randomSortDirection} from "../utils.js"
-import {MongoClient} from "mongodb"
+import { Task } from "../Task.js"
+import { createRandomIndexDoc, generateDataset } from "../utils.js"
+import { MongoClient } from "mongodb"
 
-export default class BadIdxNumIdxes extends Task{
-  constructor({targetDatabase}){
-    super({targetDatabase})
+export default class BadIdxNumIdxes extends Task {
+  constructor({ targetDatabase }) {
+    super({ targetDatabase })
     this.client = new MongoClient(this.targetDatabse.uri)
   }
   async setup(
-  ){
+  ) {
     await this.client.connect()
-    this.testDb = await this.client.db("test")
+    this.testDb = this.client.db("test")
     this.usersCollection = await testDb.collection("users")
+    this.taskCollection = await testDb.collection("task")
+
+    await this.taskCollection.dropCollection()
+    await this.taskCollection.insertOne({
+      "task": "Chief developer complaint is that inserts and updates are running slowly on the users collection"
+    })
 
     await this.usersCollection.dropCollection()
 
-    
-  let dataset1 = generateDataset({
-      numDocs: 10_000,
-      numBatches: 10,
+    for (let i = 0; i < 25; i++) {
+      db.usersCollection.createIndex(createRandomIndexDoc())
+    }
+
+
+  }
+  async run() {
+
+    let dataset = generateDataset({
+      numDocs: 1_000_000,
+      numBatches: 100,
       typeOfData: "person",
       additionalParams: {
         numWallets: 5,
         numAddress: 10
       }
     })
-   
-  for (let i = 0; i < 25; i++) {
-    db.usersCollection.createIndex(createRandomIndexDoc())
-    }
-  }
-  async run(){}
-  async cleanup(){}
-  async execute(){}
 
+    for (let batch of dataset) {
+      await this.usersCollection.insertMany(batch)
+    }
+
+    let bulkWrites = []
+
+    let opChoices = ["updateMany", "updateOne", "deleteOne"]
+    for (let x = 0; x < 200; x++) {
+      let operationDoc = {}
+
+      let opChoice = opChoices[Math.floor(Math.random() * opChoices.length + 1)]
+
+      switch (opChoice) {
+        case "updateMany":
+          operationDoc = {
+            updateMany: {
+              filter: {
+                accountTotal: { $lt: 300 }
+              },
+              update: {
+                $inc: { accountTotal: 20 }
+              }
+            }
+          }
+          break;
+        case "updateOne":
+          operationDoc = {
+            updateOne: {
+              filter: {
+                accountTotal: { $lt: 300 }
+              },
+              update: {
+                $mul: { accountTotal: 20 }
+              }
+            }
+          }
+          break;
+        case "deleteOne":
+          operationDoc = {
+            deleteOne: {
+              filter: { $lt: 10_000 }
+            }
+          }
+          break;
+      }
+
+      bulkWrites.push(operationDoc)
+    }
+
+    await this.usersCollection.bulkWrite(bulkWrites, { ordered: false })
   }
+
+}
